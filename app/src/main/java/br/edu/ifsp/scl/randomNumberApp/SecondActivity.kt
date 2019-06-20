170586300330a12780381f61d533830db0e3fe49
Haroldo Leite <holiverleite@gmail.com>
2019-06-20 15:29:12 -0300

    Implementation simple navigation between two activities and passing values through them.

---
+++ app/src/main/java/br/edu/ifsp/scl/randomNumberApp/SecondActivity.kt
@@ -0,0 +1,34 @@
+package br.edu.ifsp.scl.randomNumberApp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber(view: View? = null) {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        val random = Random()
+        var randomInt = 0
+
+        if (count > 0) {
+            randomInt = random.nextInt(count + 1)
+        }
+
+        findViewById<TextView>(R.id.textview_random).text = Integer.toString(randomInt)
+        findViewById<TextView>(R.id.textview_label).text = getString(R.string.random_heading, count)
+    }
+}
